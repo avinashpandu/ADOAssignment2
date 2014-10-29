fab4aa9ffaf8f03f7f33d9af8449793237295f90
Karthik Narisetti <knariset@hawk.iit.edu>
2014-10-29 14:16:12 -0600

    Statistical functions till 29/10
    Commit 29/10

--- buffer_mgr.c
+++ buffer_mgr.c
@@ -4,43 +4,228 @@
 // for type bool
 #include "dt.h"
 
+// read and write tags 
 RC rtag=RC_OK,wtag=RC_OK;
+
+frame_content *tempframe=NULL;
+
+// for mutexes
+static pthread_mutex_t lock_initbufferpool=PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t lock_shutdown=PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t lock_forceflush=PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t lock_pin=PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t lock_unpin=PTHREAD_MUTEX_INITIALIZER;
+
+// Head node pointer
+pointer_to_node head_ptr=NULL;
+
 
 RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, 
 		  const int numPages, ReplacementStrategy strategy, 
 		  void *stratData)
 {
+RC rc=RC_OK;
+int ctr;
+page_Frame *node_of_pool;
+frame_content *frame;
+BM_BufferPool *local;
+	
+// create a file handle too
+SM_FileHandle *fh;
+bool check;
 // Obtain a lock on the code block
+pthread_mutex_lock(&lock_initbufferpool);
+
 //Check if the buffer pool exists
-//Check for pointer existence,if null throw an error.
-//If false,allocate memory to the buffer pool based on the no of pages passed.
-//Call the insert method from linked list and create the number of page frames based on the pages passed.
-//Check the return code of insert ,if true,throw sucess else page frame cannot be intialised
-//Set the parameters of bm
-// unlock the block
+node_of_pool = what_pools_active(pageFileName,head_ptr);
+
+//Check for pointer existence,if null then create a pool of required 
+	if(node_of_pool == NULL)
+    {
+        fh=(SM_FileHandle *)malloc(sizeof(SM_FileHandle));
+        if(fh==NULL)
+        return RC_FILE_HANDLE_NOT_INIT;
+        openPageFile(pageFileName,sh);
+		
+		// assign all parameters to 
+        bm->pageFile=pageFileName;
+        bm->numPages=numPages;
+        bm->strategy=strategy;
+        bm->mgmtData=fh;
+
+        //initialize a frame with passed parameters
+		tempframe = (frame_content *) malloc(numPages,sizeof(frame_content));
+		//If false,allocate memory to the buffer pool based on the no of pages passed.
+		if (tempframe !=NULL)
+		{
+			for (ctr=0;ctr<numPages;ctr++)
+			{
+			(tempframe+ctr)->frame = initpf (numPages);
+			tempframe[i].dirty_flag = false;
+			tempframe[i].fix_count=0;
+			tempframe[i].pageno = -1;
+			}
+		}
+		frame = tempframe;
+		
+		if (frame != NULL)
+        rc = RC_OK;
+		else 
+		rc = RC_FILE_HANDLE_NOT_INIT;
+		
+		//Check the return code of insert ,if true,throw success else page frame cannot be intialised
+        check = insert_node(bm,frame,&head_ptr);
+		if (check == FALSE)
+        rc=RC_FILE_HANDLE_NOT_INIT;
+    }
+	else
+	{
+	local = node_of_pool-> pool_Pointer;
+	//Set the parameters of bm
+	bm->numPages = numPages;
+	bm->strategy = strategy;
+	bm->mgmtData = tempframe->mgmtData;
+	frame_content = node_of_pool->frame_content;
+	
+	//Call the insert method from linked list and create the number of page frames based on the pages passed.
+	check = insert_node(bm,frame,&head_ptr);
+	if (check == FALSE)
+    rc=RC_FILE_HANDLE_NOT_INIT;
+	}
+
+// unlock the block	
+pthread_mutex_unlock(&lock_initbufferpool);
+return rc;
 }
+
+
 RC shutdownBufferPool(BM_BufferPool *const bm)
 {
-// Obtain a lock on the code block
+RC rc=RC_OK;
+int *fixc,*gfc,i,accessctr;
+page_Frame *pf;
+frame_content *fc;
+char *fr;
 //set that the page is not pinned i.e. it is false
+bool pin = FALSE;
+// Obtain a lock on the code block
+pthread_mutex_lock(&lock_shutdown);
+
 //get the fixed counts
+fixc=getFixCounts(bm);
 //get the frame contents
-//if the fix count is greater than zero then,means that the page has been pinned.
-//call the respective method to check if the page is dirty and write back to disk
-// Free the memory that has been allocated to the buffer pool.
-// Unlock
+gfc=getFrameContents(bm);
+
+for(i=0 ; i < bm->numPages ; i++)
+{
+	//if the fix count is greater than zero then,means that the page has been pinned.
+    if(fixounts[i] > 0)
+    pin=TRUE;
 }
+//check if the page is dirty and write back to disk
+if (pin == FALSE)
+{
+	pf = search_Page_Frame(bm,head_ptr);
+	accessctr = pages_of_file_Associated(bm->pageFile,head_ptr);
+	if (pf!=NULL)
+	{
+	fc = pf->frame_content;
+		if (fc!=NULL)
+		{
+			for (i=0; i< bm->numPages; i++)
+			{
+			fr = fc[i].frame;
+				if (fr[i].dirty_flag== TRUE)
+				{
+					if (writeBlock(fc[i].pageno,bm->mgmtData,fr) == RC_OK)
+					rc = RC_OK;
+					else
+					rc = RC_WRITE_FAILED;
+				}
+			// Check if pool associated only one file and if yes then free frame
+			if (accessctr == 1)
+			free (fr);
+			}
+		}
+		if (accessctr == 1)
+		free (fc);
+		fc = NULL;
+		delete_Page_Frame (bm,&head_ptr);
+	}
+	// Close Page File that is associated with it
+	if (accessctr == 1)
+	{
+	closePageFile(bm->mgmtData);
+	// Free the memory that has been allocated to the buffer pool.
+	free (bm->mgmtData);
+	}
+}
+// Unlock
+pthread_mutex_unlock(&lock_shutdown);
+return rc;
+}
+
+
 RC forceFlushPool(BM_BufferPool *const bm)
 {
+RC rc=RC_OK;
+// Local Variables
+char *fr;
+frame_content *fc;
+page_Frame *pf;
 // Obtain a lock on the code block
-//call the respective method to check if the page is dirty and write back to disk}
-// unlock
+pthread_mutex_lock (&lock_forceflush);
+pf = search_Page_Frame(bm,head_ptr);
+if (pf!=NULL)
+{
+fc = pf->frame_content;
+	if (fc!=NULL)
+	{
+		for (i=0; i< bm->numPages; i++)
+		{
+		fr = fc[i].frame;
+		// Check if the page is dirty and write back to disk}
+			if (fr[i].dirty_flag== TRUE)
+			{
+				if (writeBlock(fc[i].pageno,bm->mgmtData,fr) == RC_OK)
+				rc = RC_OK;
+				else
+				rc = RC_WRITE_FAILED;
+				fc.dirty_flag = FALSE;
+				pf->pages_wrote++;
+			}
+		}
+	}
 }
+// unlock
+pthread_mutex_unlock(&lock_forceflush);
+return rc;
+}
+
 // Buffer Manager Interface Access Pages
 RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page)
 {
+RC rc=RC_OK;
+// Local variables
+page_Frame *pf;
+frame_content *fc;
+int i;
+pf = search_Page_Frame(bm,head_ptr);
+fc = pf->frame_content;
 //Traverse the contents of the page frame from the 1st node till last.If dirty,RC_OK else,mark dirty failed error. 
+for (i=0;i<bm->numPages;i++)
+{
+	if (((fc+i)->pageno) == page->pageNum)
+	{
+	(fc+i)->dirty_flag = TRUE;
+	rc = RC_OK;
+	}
+rc = RC_WRITE_FAILED;
 }
+return rc;
+}
+
+
 RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page)
 {
 // Check for the requested page in buffer
@@ -50,12 +235,14 @@ RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page)
 // if not dirty then unpin directly
 // if not zero then throw error saying page cant be unpinned
 }
+
 RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page)
 {
 // Check for page frame existence
 // Check if the page is dirty.If yes,then write it to the page file.
 // #if error file not found.
 }
+
 RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
 	    const PageNumber pageNum)
 {
@@ -74,40 +261,113 @@ RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page,
 // Statistics Interface
 PageNumber *getFrameContents (BM_BufferPool *const bm)
 {
+PageNumber *page_array;
+int i;
+//Local Variables
+frame_content *fc;
+pointer_to_node *ptn;
 //check for the frame existence.
-//allocate memory to the integer array
+ptn = search_Page_Frame (head_ptr,bm);
+//allocate memory to the boolean array
+page_array = (PageNumber *) malloc(bm->numPages,sizeof(PageNumber));
+if (ptn !=NULL)
+{
 //traverse till the end of page frame.
-//return the array 
+fc = ptn->frame_content;
+	if (fc!=NULL)
+	{
+	//Check if the page is dirty.if yes,set true
+		for (i=0;i<bm->numPages;i++)
+		page_array[i]=fc[i].pageno;
+	}
 }
+//return the array 
+return page_array;
+}
+
 bool *getDirtyFlags (BM_BufferPool *const bm)
 {
+bool *dirty_array;
+int i;
+//Local Variables
+frame_content *fc;
+pointer_to_node *ptn;
 //check for the frame existence.
-//allocate memory to the integer array
+ptn = search_Page_Frame (head_ptr,bm);
+//allocate memory to the boolean array
+dirty_array = (bool *) malloc(bm->numPages,sizeof(bool));
+if (ptn !=NULL)
+{
 //traverse till the end of page frame.
-//Check if the page is dirty.if yes,set true
-//return the array 
+fc = ptn->frame_content;
+	if (fc!=NULL)
+	{
+	//Check if the page is dirty.if yes,set true
+		for (i=0;i<bm->numPages;i++)
+		dirty_array[i]=fc[i].dirty_flag;
+	}
 }
+//return the array 
+return dirty_array;
+}
+
 int *getFixCounts (BM_BufferPool *const bm)
 {
+int i,*fc_array;
+//Local Variables
+pointer_to_node *ptn;
+frame_content *fc;
 //check for the frame existence.
+ptn = search_Page_Frame (head_ptr,bm);
 //allocate memory to the integer array
+fc_array = (int *) malloc(bm->numPages,sizeof (int));
+	
+if (ptn !=NULL)
+{
 //traverse till the end of page frame.
-//Check the fix count of the pages in the page frame .
-//return the array 
+fc = ptn->frame_content;
+	if (fc!=NULL)
+	{
+	//Check the fix count of the pages in the page frame .
+		for (i=0;i<bm->numPages;i++)
+		fc_array[i]=fc[i].fix_count;
+	}
 }
+//return the array 
+return fc_array;
+}
+
 int getNumReadIO (BM_BufferPool *const bm)
 {
+int i;
+pointer_to_node *ptn;
 // Search for the node that was requested for
+ptn = search_Page_Frame (head_ptr,bm);
 // When found get the no of pages read
 // if not return zero
+if (ptn == NULL)
+i =0;
 // If found return no of pages
+else
+i= ptn->pages_read;
+return i;
 }
+
+
 int getNumWriteIO (BM_BufferPool *const bm);
 {
+int i;
+pointer_to_node *ptn;
 // Search for the node that was requested for
-// When found get the no of pages wrote
+ptn = search_Page_Frame (head_ptr,bm);
+// When found get the no of pages write
 // if not return zero
+if (ptn == NULL)
+i =0;
 // If found return no of pages
+else
+i= ptn->pages_wrote;
+return i;
 }
 
 // The algorithm must return whether it is success or not
@@ -208,4 +468,36 @@ bool doalgorithm(BM_BufferPool * const bm, const PageNumber pageNum, BM_PageHand
 	return true; 
 	else
 	return false;
+}
+
+// Check pools that are active
+page_Frame *what_pools_active(char *filename,pointer_to_node head_ptr)
+{
+    pointer_to_node prev_ptr=NULL;
+    pointer_to_node curr_ptr=head_ptr;
+	
+    BM_BufferPool *bp;
+	
+    while(curr_ptr!=NULL)
+    {
+        bp=(BM_BufferPool *)curr_ptr->pool_Pointer;
+        if(bp->pageFile==filename)
+        break;
+        prev_ptr = curr_ptr;
+        curr_ptr = curr_ptr->next_Page_Frame;
+    }
+    return curr_ptr;
+}
+
+// To allocate a page
+char *initpf (const int np)
+{
+int i;
+char *page;
+page = (char *)malloc (PAGE_SIZE);
+	if (page != NULL)
+	{
+	for(i=0;i < PAGE_SIZE ; i++)
+    page[i]='\0';
+    }
 }
